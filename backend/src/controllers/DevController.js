03e16f0cc053060369f5780f93ebb04d8be87efb
MaduFernandes <eduarda.alves.siqueira.fernandes>
2020-01-23 15:02:44 -0300

    removendo duplicaçao de usuarios

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,27 +5,31 @@ module.exports = {
   async store(request, response) {
     const { github_user, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_user}`
-    );
+    let dev = await Dev.findOne({ github_user });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_user}`
+      );
 
-    const techsArray = techs.split(",").map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      const techsArray = techs.split(",").map(tech => tech.trim());
 
-    const dev = await Dev.create({
-      github_user,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_user,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return response.json(dev);
   }
